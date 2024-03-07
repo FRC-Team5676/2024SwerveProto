04b5aa6980d68bd2d52100ea4d6f675e303748f4
Eric Macy <illinihiker@outlook.com>
2024-03-07 18:13:17 -0500

    Encoder changes

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -5,6 +5,8 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
@@ -22,15 +24,12 @@ public class SwerveModule extends SubsystemBase {
         public final CANSparkMax m_driveSparkMax;
         public final CANSparkMax m_turnSparkMax;
         public final RelativeEncoder m_driveEncoder;
-        public final CANcoder m_turnCANcoder;
-        public final RelativeEncoder m_turnEncoder;
+        public final AbsoluteEncoder m_turnEncoder;
         public final double m_turnEncoderOffsetDeg;
-        public final double m_turnAngleCorrectionRad;
         public final ModulePosition m_modulePosition;
 
         public boolean m_driveMotorConnected;
         public boolean m_turnMotorConnected;
-        public boolean m_turnCoderConnected;
 
         private SwerveModulePosition m_currentPosition = new SwerveModulePosition();
         private SwerveModuleState m_currentState = new SwerveModuleState();
@@ -55,17 +54,9 @@ public class SwerveModule extends SubsystemBase {
                 m_driveSparkMax.restoreFactoryDefaults();
                 m_turnSparkMax.restoreFactoryDefaults();
 
-                // turn absolute encoder setup
-                m_turnEncoderOffsetDeg = turnEncoderOffsetDeg;
-                m_turnCANcoder = new CANcoder(cancoderCanChannel);
-                CANcoderConfiguration canConfig = new CANcoderConfiguration();
-                canConfig.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
-                canConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
-                m_turnCANcoder.getConfigurator().apply(canConfig);
-
                 // Setup encoders and PID controllers for the driving and turning SPARKS MAX.
                 m_driveEncoder = m_driveSparkMax.getEncoder();
-                m_turnEncoder = m_turnSparkMax.getEncoder();
+                m_turnEncoder = m_turnSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
                 m_drivePIDController = m_driveSparkMax.getPIDController();
                 m_turnPIDController = m_turnSparkMax.getPIDController();
                 m_drivePIDController.setFeedbackDevice(m_driveEncoder);
@@ -113,8 +104,7 @@ public class SwerveModule extends SubsystemBase {
                 m_driveSparkMax.burnFlash();
                 m_turnSparkMax.burnFlash();
 
-                // Calc Relative Encoder Correction
-                m_turnAngleCorrectionRad = Math.abs(getAbsolutePositionRad());
+                m_turnEncoderOffsetDeg = turnEncoderOffsetDeg;
 
                 m_driveEncoder.setPosition(0);
 
@@ -122,20 +112,18 @@ public class SwerveModule extends SubsystemBase {
 
                 ShuffleboardContent.initDriveShuffleboard(this);
                 ShuffleboardContent.initTurnShuffleboard(this);
-                ShuffleboardContent.initCANCoderShuffleboard(this);
                 ShuffleboardContent.initBooleanShuffleboard(this);
-                ShuffleboardContent.initCoderBooleanShuffleboard(this);
         }
 
         public void setDesiredState(SwerveModuleState desiredState) {
                 // Apply chassis angular offset to the desired state.
                 SwerveModuleState correctedDesiredState = new SwerveModuleState();
                 correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
-                correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromRadians(m_turnAngleCorrectionRad));
+                correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromDegrees(m_turnEncoderOffsetDeg));
 
                 // Optimize the reference state to avoid spinning further than 90 degrees.
                 SwerveModuleState optimizedDesiredState = SwerveModuleState.optimize(correctedDesiredState,
-                                new Rotation2d(m_turnEncoder.getPosition() + m_turnAngleCorrectionRad));
+                                new Rotation2d(m_turnEncoder.getPosition()));
 
                 // Command driving and turning SPARKS MAX towards their respective setpoints.
                 m_drivePIDController.setReference(optimizedDesiredState.speedMetersPerSecond,
@@ -161,20 +149,17 @@ public class SwerveModule extends SubsystemBase {
         private boolean checkCAN() {
                 m_driveMotorConnected = m_driveSparkMax.getFirmwareVersion() != 0;
                 m_turnMotorConnected = m_turnSparkMax.getFirmwareVersion() != 0;
-                m_turnCoderConnected = m_turnCANcoder.getDeviceID() != 0;
 
-                return m_driveMotorConnected && m_turnMotorConnected && m_turnCoderConnected;
+                return m_driveMotorConnected && m_turnMotorConnected;
         }
 
         public double getAbsolutePositionRad() {
-                double absPosRad = Units.rotationsToRadians(m_turnCANcoder.getAbsolutePosition().getValueAsDouble());
-                absPosRad -= Units.degreesToRadians(m_turnEncoderOffsetDeg);
-                return absPosRad;
+                return m_turnEncoder.getPosition();
         }
 
         public double getTurnPositionRad() {
                 double conv = 0;
-                double fullPosDeg = Units.radiansToDegrees(m_turnEncoder.getPosition() + m_turnAngleCorrectionRad);
+                double fullPosDeg = Units.radiansToDegrees(m_turnEncoder.getPosition());
 
                 if (fullPosDeg < 0)
                         conv = 360;
--- src/main/java/frc/robot/utils/ShuffleboardContent.java
+++ src/main/java/frc/robot/utils/ShuffleboardContent.java
@@ -5,9 +5,6 @@
 package frc.robot.utils;
 
 import java.util.Map;
-
-import com.ctre.phoenix6.StatusSignal;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -64,42 +61,11 @@ public class ShuffleboardContent {
                                 .getLayout(turnLayout, BuiltInLayouts.kList).withPosition(moduleNumber * 2, 2)
                                 .withSize(2, 3).withProperties(Map.of("Label position", "LEFT"));
 
-                tuLayout.addNumber("TurnEncPosDeg" + abrev, () -> Units.radiansToDegrees(sm.getTurnPositionRad()));
-                tuLayout.addNumber("ActAngDeg" + abrev, () -> Units.radiansToDegrees(sm.m_turnEncoder.getPosition()));
                 tuLayout.addNumber("TurnAngleOut" + abrev, () -> sm.m_turnSparkMax.getAppliedOutput());
-                tuLayout.addNumber("AbsPosDeg" + abrev, () -> Units.rotationsToDegrees(sm.m_turnCANcoder.getAbsolutePosition().getValueAsDouble()));
+                tuLayout.addNumber("AbsPosDeg" + abrev, () -> Units.radiansToDegrees(sm.m_turnEncoder.getPosition()));
                 tuLayout.addNumber("CurrentAmps" + abrev, () -> sm.m_turnSparkMax.getOutputCurrent());
                 tuLayout.addNumber("AbsOffsetDeg" + abrev, () -> sm.m_turnEncoderOffsetDeg);
                 tuLayout.addNumber("Firmware" + abrev, () -> sm.m_turnSparkMax.getFirmwareVersion());
-        }
-
-        public static void initCoderBooleanShuffleboard(SwerveModule sm) {
-                int moduleNumber = sm.m_modulePosition.ordinal();
-                String abrev = ModuleConstants.modAbrev[moduleNumber];
-                ShuffleboardTab x = Shuffleboard.getTab("CanCoders");
-
-                x.addBoolean("Connected" + abrev, () -> sm.m_turnCoderConnected)
-                                .withPosition(moduleNumber * 2 - 1, 4); // 1 3 5 7
-
-                StatusSignal<Integer> faults = sm.m_turnCANcoder.getFaultField();
-                x.addBoolean("Faults" + abrev, () -> 0 != faults.getValue())
-                                .withPosition(moduleNumber * 2, 4); // 2 4 6 8
-
-        }
-
-        public static void initCANCoderShuffleboard(SwerveModule sm) {
-                int moduleNumber = sm.m_modulePosition.ordinal();
-                String abrev = ModuleConstants.modAbrev[moduleNumber];
-                String canCoderLayout = sm.m_modulePosition.toString() + " CanCoder";
-
-                ShuffleboardLayout coderLayout = Shuffleboard.getTab("CanCoders")
-                                .getLayout(canCoderLayout, BuiltInLayouts.kList).withPosition(moduleNumber * 2, 0)
-                                .withSize(2, 3).withProperties(Map.of("Label position", "LEFT"));
-
-                coderLayout.addNumber("AbsPosDeg" + abrev, () -> Units.rotationsToDegrees(sm.m_turnCANcoder.getAbsolutePosition().getValueAsDouble()));
-                coderLayout.addNumber("AbsOffsetDeg" + abrev, () -> sm.m_turnEncoderOffsetDeg);
-                coderLayout.addNumber("PosDeg" + abrev, () -> Units.rotationsToDegrees(sm.m_turnCANcoder.getPosition().getValueAsDouble()));
-                coderLayout.addNumber("VelocityRps" + abrev, () -> sm.m_turnCANcoder.getVelocity().getValueAsDouble());
         }
 
         public static void initGyro(SwerveDrive sd) {
